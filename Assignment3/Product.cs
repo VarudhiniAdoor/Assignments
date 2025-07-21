6a884f02f41e1cadfcd331dc6a5d66504597f881
Varudhini <varudhinia12@gmail.com>
2025-07-21 12:47:26 +0530

    Assignment 3 updation

---
+++ Assignment3/Product.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment3
+{
+    public class Product
+    {
+        private readonly string pcode;
+        private string pname;
+        private int qtyInStock;
+        private int discountAllowed;
+        private static string brand = "CGI";
+
+        private static List<Product> productList = new List<Product>();
+
+
+        public Product(string pcode, string pname, int qty, int discount)
+        {
+            this.pcode = pcode;
+            this.pname = pname;
+            this.qtyInStock = qty;
+            this.discountAllowed = discount;
+        }
+
+
+        public static void AddProduct()
+        {
+
+            Console.Write("Enter Product Code: ");
+            string pcode = Console.ReadLine();
+
+            Console.Write("Enter Product Name: ");
+            string pname = Console.ReadLine();
+
+            Console.Write("Enter Quantity in Stock: ");
+            int qty = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Discount Allowed (%): ");
+            int discount = int.Parse(Console.ReadLine());
+
+            Product p = new Product(pcode, pname, qty, discount);
+            productList.Add(p);
+
+            Console.WriteLine("Product Added Successfully!\n");
+        }
+
+
+        public static void DisplayProducts()
+        {
+            Console.WriteLine("\n--- Product List ---");
+            Console.WriteLine($"Brand: {brand}"); //brand
+
+            foreach (var p in productList)
+            {
+                Console.WriteLine($"Code: {p.pcode}, Name: {p.pname}, Stock: {p.qtyInStock}, Discount: {p.discountAllowed}%");
+            }
+        }
+
+        public static void OrderProduct()
+        {
+
+            Console.Write("\nEnter Product Name to Purchase: ");
+            string searchName = Console.ReadLine();
+
+            Product found = null;
+
+            foreach (var p in productList)
+            {
+                if (p.pname.Equals(searchName))
+                {
+                    found = p;
+                    break;
+                }
+            }
+
+            if (found == null)
+            {
+                Console.WriteLine("Product Not Found.");
+                return;
+            }
+
+            Console.WriteLine($"Available Stock: {found.qtyInStock}");
+            Console.Write("Enter Quantity: ");
+            int qty = Convert.ToInt32(Console.ReadLine());
+
+            if (qty > found.qtyInStock)
+            {
+                Console.WriteLine("Insufficient Stock.");
+                return;
+            }
+
+            double pricePerUnit = 100;  //is it right to imagine our own price amount? How else are we supposed to calculate discount?
+            double total = pricePerUnit * qty;
+            double discount = total * 0.5;
+            double payable = total - discount;
+
+            Console.WriteLine("\n--- BILL ---");
+            Console.WriteLine($"Brand: {brand}");
+            Console.WriteLine($"Product: {found.pname}");
+            Console.WriteLine($"Quantity: {qty}");
+            Console.WriteLine($"Original Price: ₹{total}");
+            Console.WriteLine($"Discount: ₹{discount} as today is 26th Jan, Company wants to give a discount of 50% on all the products");
+            Console.WriteLine($"Final Amount: ₹{payable}");
+        }
+    }
+}
---
+++ Assignment3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Admin");
+                Console.WriteLine("2. Customer");
+                Console.WriteLine("3. Exit");
+
+                Console.Write("Enter Choice: ");
+                int role = int.Parse(Console.ReadLine());
+
+                if (role == 1)
+                {
+                    Console.WriteLine("1. Add Product");
+                    Console.WriteLine("2. Display Products");
+
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 1)
+                        Product.AddProduct();
+                    else if (choice == 2)
+                        Product.DisplayProducts();
+
+                }
+                else if (role == 2)
+                {
+                    Product.OrderProduct();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
