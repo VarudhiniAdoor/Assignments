54604dde4c161db34469aa29978bf094f84b2fe0
Varudhini <varudhinia12@gmail.com>
2025-07-24 09:38:14 +0530

    added 10 changes

--- Assignment 10/Program.cs
+++ Assignment 10/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Sum: " + Operation.Sum(1, 2, null, 4));
+                Console.WriteLine("Product: " + Operation.Product(1, null, 3));
+                Console.WriteLine("Min: " + Operation.Min(100, null, 1, 20));
+                Console.WriteLine("Max: " + Operation.Max(100, null, 1, 20));
+                Console.WriteLine("Is 17 Prime? " + Operation.IsPrime(17));
+
+                int start = 1, end = 10;
+                start.DisplayEven(end);
+                start.DisplayOdd(end);
+                start.DisplayPrimes(end);
+                5.DisplayTable();
+                start.DisplayTables1To10(end);
+                1.DisplayTablesInRange( 7, 2, 4);
+                Console.WriteLine("Reverse of 12345: " + 12345.ReverseNumber());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
         }
     }
 }
